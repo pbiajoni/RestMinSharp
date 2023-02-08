0f872246f8d0103595ab6b1abcb5279c945312fb
Pablo Biajoni <pbiajoni@hotmail.com>
2023-02-08 10:26:47 -0300

    Adicionado downlod de memory stream

---
+++ RestMinSharp/Results/MemoryStreamRequestResult.cs
@@ -0,0 +1,15 @@
+﻿using RestMinSharp.Notifications;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RestMinSharp.Results
+{
+    public class MemoryStreamRequestResult
+    {
+        public bool IsAuthorized { get; internal set; }
+        public List<Notification> Notifications { get; set; } = new List<Notification>();
+        public bool HasNotifications { get => Notifications.Any(); }
+        public MemoryStream Stream { get; set; }
+    }
+}
