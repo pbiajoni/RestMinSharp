2896248a1c447356f158dc645f9e028d7f92a19b
Pablo Biajoni <pbiajoni@hotmail.com>
2023-02-25 09:37:43 -0300

    Adicionado raw response

--- RestMinSharp/Results/RequestResult.cs
+++ RestMinSharp/Results/RequestResult.cs
@@ -6,6 +6,7 @@ namespace RestMinSharp.Results
 {
     public class RequestResult<T>
     {
+        public object RawData { get; set; }
         public bool IsAuthorized { get; internal set; }
         public List<Notification> Notifications { get; set; } = new List<Notification>();
         public bool HasNotifications { get => Notifications.Any(); }
